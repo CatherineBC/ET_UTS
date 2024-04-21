460fe3b068a96e74924388ea3d36dd09a53cb302
CatherineBC <catherinehuangbc@gmail.com>
2024-04-21 18:20:04 +0700

    Update  class

--- memorimage_160421082_160421046/lib/class/question.dart
+++ memorimage_160421082_160421046/lib/class/question.dart
@@ -1,12 +1,11 @@
 class QuestionObj {
-  String image;
   String option_a;
   String option_b;
   String option_c;
   String option_d;
   String answer;
 
-QuestionObj(this.image, this.option_a,
+QuestionObj(this.option_a,
 this.option_b, this.option_c, this.option_d, this.answer);
 
 }
